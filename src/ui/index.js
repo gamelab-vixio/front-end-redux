43229d4d9c4f6379e37098a999a7c85b5567dac5
darmawanalbert <darmawanalbert@gmail.com>
2018-09-09 17:11:57 +0700

    Add ProfileCard components

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,8 +1,7 @@
 // Design Token
 export { default as Token } from './Token';
 
-// Primitive Components
-export { default as RatingStars } from './primitives/RatingStars';
-export { default as LoadingScreen } from './primitives/LoadingScreen';
-
-// Non-primitive Components
+// Components
+export { default as RatingStars } from './components/RatingStars';
+export { default as LoadingScreen } from './components/LoadingScreen';
+export { default as ProfileCard } from './components/ProfileCard';

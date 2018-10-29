a188912a886266531fcb3eea048b8250fef35225
shan <shanvaldo@gmail.com>
2018-10-29 22:09:27 +0700

    Fix main title scss in every main page

--- src/ui/components/Title.js
+++ src/ui/components/Title.js
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 class Title extends Component {
   static propTypes = {
     text: PropTypes.string.isRequired,
-  }
+  };
 
   render() {
     const { text } = this.props;
     return (
       <div>
-        <h1 className="login-title">{text}</h1>
+        <h1 className="main-title">{text}</h1>
         <hr className="styled-line" />
       </div>
     );
